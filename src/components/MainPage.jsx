5bf0efe2d0aaf48efc0258ea773ce6eaaf01e431
Ethan Haneklaus <ethanhaneklaus@gmail.com>
2022-08-02 18:41:10 -0500

    adjusting main page columns

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -9,38 +9,161 @@ function MainPage() {
         <div className="maincontainer">
 
             <div className="leftcontainer">
-                <div className="test">Suggested
 
+                <h8>Suggested for you</h8>
+
+                <div className="rowcontainer">
+                    <div className="columncontainer">
+                        <>
+                            <iframe
+                                height="615"
+                                width="325"
+                                src="https://www.tiktok.com/embed/7119842526156033326"
+                                allowfullscreen
+                                scrolling="no"
+                                allow="encrypted-media;"
+                            ></iframe>
+                        </>
+                        <>
+                            <div className="card">
+                                <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="John" style={{ width: "100%" }} />
+                                <h1>Users Name</h1>
+                                <h1>Users Handle</h1>
+                                <p className="title">Biography</p>
+                                <p>Follow My Socials</p>
+                                <a href="#"><i className="bi bi-snapchat"></i></a>
+                                <a href="#"><i className="bi bi-twitter"></i></a>
+                                <a href="#"><i className="bi bi-linkedin"></i></a>
+                                <a href="#"><i className="bi bi-facebook"></i></a>
+                                <p><button className="button">Contact</button></p>
+                            </div>
+                        </>
+                    </div>
+                    <div className="columncontainer">
+                        <div className="rowcontainer">
+                            <input type="text" placeholder="Like to leave a comment?"></input>
+                            <button type="submit">Submit</button>
+                        </div>
+                        <div>
+                            populate submited text here
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <>
+                            <iframe
+                                height="615"
+                                width="325"
+                                src="https://www.tiktok.com/embed/7119518658820394282"
+                                allowfullscreen
+                                scrolling="no"
+                                allow="encrypted-media;"
+                            ></iframe>
+                        </>
+                        <>
+                            <div className="card">
+                                <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="John" style={{ width: "100%" }} />
+                                <h1>Users Name</h1>
+                                <h1>Users Handle</h1>
+                                <p className="title">Biography</p>
+                                <p>Follow My Socials</p>
+                                <a href="#"><i className="bi bi-snapchat"></i></a>
+                                <a href="#"><i className="bi bi-twitter"></i></a>
+                                <a href="#"><i className="bi bi-linkedin"></i></a>
+                                <a href="#"><i className="bi bi-facebook"></i></a>
+                                <p><button className="button">Contact</button></p>
+                            </div>
+                        </>
+                    </div>
+                    <div width="325">
+                        <input type="text" placeholder="Like to leave a comment?"></input>
+                        <button type="submit">Submit</button>
+                        <div>
+                            populate submited text here
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div className="middlecontainer">
 
-                <div ClassName="tiktokdiv">
-                    <iframe
-                        src="https://www.tiktok.com/embed/7115393853708094254"
-                        allowfullscreen
-                        scrolling="no"
-                        allow="encrypted-media;"
-                    ></iframe>
-                </div>
-
-
+                <h8>You're Following</h8>
 
                 <div>
-                    Comment Section
+                    <div>
+                        <>
+                            <iframe
+                                height="615"
+                                width="325"
+                                src="https://www.tiktok.com/embed/7120440518013734187"
+                                allowfullscreen
+                                scrolling="no"
+                                allow="encrypted-media;"
+                            ></iframe>
+                        </>
+                        <>
+                            <div className="card">
+                                <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="John" style={{ width: "100%" }} />
+                                <h1>Users Name</h1>
+                                <h1>Users Handle</h1>
+                                <p className="title">Biography</p>
+                                <p>Follow My Socials</p>
+                                <a href="#"><i className="bi bi-snapchat"></i></a>
+                                <a href="#"><i className="bi bi-twitter"></i></a>
+                                <a href="#"><i className="bi bi-linkedin"></i></a>
+                                <a href="#"><i className="bi bi-facebook"></i></a>
+                                <p><button className="button">Contact</button></p>
+                            </div>
+                        </>
+                    </div>
+                    <div width="325">
+                        <input type="text" placeholder="Like to leave a comment?"></input>
+                        <button type="submit">Submit</button>
+                        <div>
+                            populate submited text here
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-            <div className="leftcontainer">
-                <div className="test">User Info
-
+                <div>
+                    <>
+                        <iframe
+                            height="615"
+                            width="325"
+                            src="https://www.tiktok.com/embed/7124128907141418282"
+                            allowfullscreen
+                            scrolling="no"
+                            allow="encrypted-media;"
+                        ></iframe>
+                    </>
+                    <>
+                        <div className="card">
+                            <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="John" style={{ width: "100%" }} />
+                            <h1>Users Name</h1>
+                            <h1>Users Handle</h1>
+                            <p className="title">Biography</p>
+                            <p>Follow My Socials</p>
+                            <a href="#"><i className="bi bi-snapchat"></i></a>
+                            <a href="#"><i className="bi bi-twitter"></i></a>
+                            <a href="#"><i className="bi bi-linkedin"></i></a>
+                            <a href="#"><i className="bi bi-facebook"></i></a>
+                            <p><button className="button">Contact</button></p>
+                        </div>
+                    </>
+                    <div width="325">
+                        <input type="text" placeholder="Like to leave a comment?"></input>
+                        <button type="submit">Submit</button>
+                        <div>
+                            populate submited text here
+                        </div>
+                    </div>
                 </div>
             </div>
 
         </div>
+
+
     );
-
 };
-
 export default MainPage;
