acd84fce6168f9b96ab9ffedf50e79815bb4a8a3
Ethan Haneklaus <ethanhaneklaus@gmail.com>
2022-08-16 16:53:19 -0500

    server and package.json for deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT ?? 8080;
+const PORT = process.env.PORT || 8080
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
     cors: {
